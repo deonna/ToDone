b589a612e0e66ff7285a382c7adeb41e545a1914
Deonna Hodges <dnhodges@gmail.com>
2017-02-14 11:10:19 -0800

    refactor(toDone): Extracted model code out of views.

--- app/src/main/java/com/deonna/todone/Codes.java
+++ app/src/main/java/com/deonna/todone/Codes.java
@@ -4,7 +4,7 @@ package com.deonna.todone;
  * Created by deonna on 2/14/17.
  */
 
-public class Codes {
+class Codes {
     public static final int EDIT_REQUEST = 1;
     public static final int EDITED = 2;
 }
