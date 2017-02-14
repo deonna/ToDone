7b444ed4fff4f514d6f10a4929d9446582a297ec
Deonna Hodges <deonna@learnersguild.org>
2017-02-13 20:35:12 -0800

    feat(todoItems): Basic setup for programmatically adding todo list items. Populating ListView with fake data.

--- app/src/main/java/com/deonna/todone/MainActivity.java
+++ app/src/main/java/com/deonna/todone/MainActivity.java
@@ -2,12 +2,35 @@ package com.deonna.todone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> todoAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        populateTodoItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(todoAdapter);
+    }
+
+    private void populateTodoItems() {
+        todoItems = new ArrayList<String>();
+
+        for(int i = 1; i < 4; i++) {
+            todoItems.add("Item " + i);
+        }
+
+        todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                todoItems);
     }
 }
