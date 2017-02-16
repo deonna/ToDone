51ddbba4d2e55f9fc3f3f2dca6eb4869bccd8d12
Deonna Hodges <deonna@learnersguild.org>
2017-02-16 15:11:41 -0800

    feat(priority): Hooked up priority UI and fixed bug where it wasn't persisting properly.

--- app/src/main/java/com/deonna/todone/TodosAdapter.java
+++ app/src/main/java/com/deonna/todone/TodosAdapter.java
@@ -6,9 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,27 +30,10 @@ public class TodosAdapter extends ArrayAdapter<Todo> {
 
         tvName.setText(todo.getName());
 
-        initializeCheckboxListener(convertView, todo);
+        Utilities.initializeCheckboxListener(convertView, todo);
         Utilities.initializePriorityListeners(convertView, todo, R.id.ivLowPriority, R.id
                 .ivMediumPriority, R.id.ivHighPriority);
 
         return convertView;
-    }
-
-    private void initializeCheckboxListener(View view, Todo todo) {
-
-        final CheckBox cbIsCompleted = (CheckBox) view.findViewById(R.id.cbIsCompleted);
-        final Todo currentTodo = todo;
-
-        cbIsCompleted.setChecked(currentTodo.getIsCompleted());
-
-        cbIsCompleted.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                currentTodo.setIsCompleted(isChecked);
-                currentTodo.updateInDataSource();
-            }
-        });
     }
 }
--- app/src/main/java/com/deonna/todone/Utilities.java
+++ app/src/main/java/com/deonna/todone/Utilities.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 public class Utilities {
@@ -12,6 +14,23 @@ public class Utilities {
 
         final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static void initializeCheckboxListener(View view, Todo todo) {
+
+        final CheckBox cbIsCompleted = (CheckBox) view.findViewById(R.id.cbIsCompleted);
+        final Todo currentTodo = todo;
+
+        cbIsCompleted.setChecked(currentTodo.getIsCompleted());
+
+        cbIsCompleted.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                currentTodo.setIsCompleted(isChecked);
+                currentTodo.updateInDataSource();
+            }
+        });
     }
 
     public static void initializePriorityListeners(View convertView, Todo todo, int lowPriorityId,
@@ -23,11 +42,15 @@ public class Utilities {
 
         final Todo currentTodo = todo;
 
+        updatePriorityUi(ivLowPriority, ivMediumPriority, ivHighPriority, todo);
+
         ivLowPriority.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
                 changePriority(ivLowPriority, ivMediumPriority);
+                currentTodo.setPriority(Priority.MEDIUM);
                 currentTodo.updateInDataSource();
             }
         });
@@ -36,7 +59,9 @@ public class Utilities {
 
             @Override
             public void onClick(View v) {
+
                 changePriority(ivMediumPriority,ivHighPriority);
+                currentTodo.setPriority(Priority.HIGH);
                 currentTodo.updateInDataSource();
             }
         });
@@ -45,14 +70,38 @@ public class Utilities {
 
             @Override
             public void onClick(View v) {
+
                 changePriority(ivHighPriority, ivLowPriority);
+                currentTodo.setPriority(Priority.LOW);
                 currentTodo.updateInDataSource();
             }
         });
     }
 
-    private static void updatePriorities() {
-        // TODO: 2/16/17 update UI for stars     
+    private static void updatePriorityUi(ImageView ivLowPriority, ImageView ivMediumPriority,
+                                         ImageView ivHighPriority, Todo todo) {
+
+        switch (todo.getPriority()) {
+            case LOW:
+                ivLowPriority.setVisibility(View.VISIBLE);
+                ivMediumPriority.setVisibility(View.GONE);
+                ivHighPriority.setVisibility(View.GONE);
+                break;
+
+            case MEDIUM:
+                ivLowPriority.setVisibility(View.GONE);
+                ivMediumPriority.setVisibility(View.VISIBLE);
+                ivHighPriority.setVisibility(View.GONE);
+                break;
+
+            case HIGH:
+                ivLowPriority.setVisibility(View.GONE);
+                ivMediumPriority.setVisibility(View.GONE);
+                ivHighPriority.setVisibility(View.VISIBLE);
+                break;
+            default:
+                break;
+        }
     }
     
     private static void changePriority(ImageView ivOldPriority, ImageView ivNewPriority) {
@@ -60,6 +109,4 @@ public class Utilities {
         ivOldPriority.setVisibility(View.GONE);
         ivNewPriority.setVisibility(View.VISIBLE);
     }
-
-
 }
